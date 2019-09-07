b939cfe9977dd96dc448e40a87fdaf4301e79a55
Eyal Kalderon <ebkalderon@gmail.com>
2019-09-07 18:45:10 +0800

    Fix broken examples, remove use of RetryPolicy

--- src/http-client.rs
+++ src/http-client.rs
@@ -4,12 +4,9 @@ use hyper::{
     Request, Response, Uri,
 };
 use std::time::Duration;
-use tower::{builder::ServiceBuilder, reconnect::Reconnect, Service, ServiceExt};
-use tower_hyper::{
-    client::{Builder, Connect},
-    retry::{Body, RetryPolicy},
-    util::Connector,
-};
+use tower::{builder::ServiceBuilder, Service, ServiceExt};
+use tower_hyper::{client::Connect, util::Connector, Body};
+use tower_reconnect::Reconnect;
 
 fn main() {
     let fut = futures::lazy(|| {
@@ -22,25 +19,20 @@ fn main() {
 
 fn request() -> impl Future<Item = Response<hyper::Body>, Error = ()> {
     let connector = Connector::new(HttpConnector::new(1));
-    let hyper = Connect::new(connector, Builder::new());
+    let hyper = Connect::new(connector);
 
-    // RetryPolicy is a very simple policy that retries `n` times
-    // if the response has a 500 status code. Here, `n` is 5.
-    let policy = RetryPolicy::new(5);
     // We're calling the tower/examples/server.rs.
     let dst = Destination::try_from_uri(Uri::from_static("http://127.0.0.1:3000")).unwrap();
 
     // Now, to build the service! We use two BufferLayers in order to:
     // - provide backpressure for the RateLimitLayer, and ConcurrencyLimitLayer
-    // - meet `RetryLayer`'s requirement that our service implement `Service + Clone`
     // - ..and to provide cheap clones on the service.
     let maker = ServiceBuilder::new()
         .buffer(5)
         .rate_limit(5, Duration::from_secs(1))
         .concurrency_limit(5)
-        .retry(policy)
         .buffer(5)
-        .make_service(hyper);
+        .service(hyper);
 
     // `Reconnect` accepts a destination and a MakeService, creating a new service
     // any time the connection encounters an error.
@@ -57,7 +49,7 @@ fn request() -> impl Future<Item = Response<hyper::Body>, Error = ()> {
         .map_err(|e| panic!("Service is not ready: {:?}", e))
         .and_then(|mut c| {
             c.call(request)
-                .map(|res| res.map(|b| b.into_inner()))
+                // .map(|res| res.map(|b| b.into_inner()))
                 .map_err(|e| panic!("{:?}", e))
         })
 }
